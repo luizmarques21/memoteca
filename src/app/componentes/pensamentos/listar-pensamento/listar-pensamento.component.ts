6480ce9c2ef24ac827c035c1db11fcfb3a685f8e
Luiz Marques <mariel.marques21@gmail.com>
2025-08-01 20:24:13 -0300

    Requisições HTTP

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {PensamentoService} from "../pensamento.service";
+import {Pensamento} from "../pensamento";
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -9,9 +11,12 @@ export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos: Pensamento[] = [];
 
-  constructor() { }
+  constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos;
+    });
   }
 
 }
